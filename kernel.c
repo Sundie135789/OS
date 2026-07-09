2b52b22f9e9685cd98642078a52d1aa37a47eede
Mudit <bhandarimudit5@gmail.com>
2026-07-09 11:13:50 +0530

    reset

--- kernel.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include "headers/framebuffer.h"
-// do input, make terminal, do draw_line from url: https://chatgpt.com/c/6a47e8c0-e044-83ee-82a6-262f531feb96
-void kernel_main(uint32_t magic, void* mb_addr){ 
-  get_framebuffer_access(magic, mb_addr);
-  clear_screen(0xFF000000);
-  print_string(200, 200, "Hello world!", 0xFFFFFFFF);
-  while(1);
-}
--- terminal.c
+++
@@ -1,5 +0,0 @@
-#include "headers/terminal.h"
-static int cursor_x = 10, cursor_y = 10;
-void update_terminal(){
-
-}
